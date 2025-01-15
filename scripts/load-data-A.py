352ede63819f958739a5acd0e9a30785838d2689
Ross Sharp <ross.sharp@dominodatalab.com>
2025-01-15 15:06:48 -0800

    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/ross_domino/MLOps_Flows_Training_Template_Dev?executionId=678835e5efde7d519a2cc723

--- scripts/load-data-A.py
+++ scripts/load-data-A.py
@@ -3,11 +3,14 @@ import shutil
 import pandas as pd
 
 # The name of the Flow input, which Domino places into a file blob under /workflow/inputs
-task_input_name = "data_path"
-input_file = f"/workflow/inputs/{task_input_name}"
+input_name = "data_path"
+input_location = f"/workflow/inputs/{input_name}"
+with open(input_location, "r") as file:
+    input_csv = file.read()
 # Read input csv to dataframe
-df = pd.read_csv(input_file) 
+df = pd.read_csv(input_csv) 
 
 
 # Write output
 df.to_csv('/workflow/outputs/datasetA.csv', index=False)
+
