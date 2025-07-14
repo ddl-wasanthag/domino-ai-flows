54e976710d5829f9ba92a53c2fef0afc3e310ed8
Ross Sharp <ross.sharp@dominodatalab.com>
2025-07-14 13:56:29 -0700

    Updating so that all task parameters are explicitly set.
    Pushed from Domino: https://lsdemo.domino-eval.com/workspace/ross_domino/domino-ai-flows-AIHUB_DEV?executionId=68755ea75750f204c7f9b5f2

--- mlops_flow_prod.py
+++ mlops_flow_prod.py
@@ -5,18 +5,21 @@ from flytekitplugins.domino.helpers import Input, Output, run_domino_job_task
 from flytekitplugins.domino.task import DominoJobConfig, DominoJobTask, GitRef, EnvironmentRevisionSpecification, EnvironmentRevisionType, DatasetSnapshot
 from flytekitplugins.domino.artifact import Artifact, DATA, MODEL, REPORT
 
-# As this is considered a PROD Flow definition, we set use_project_defaults_for_omitted to False 
+
+# As this is considered a PROD Flow definition, we do not the use_project_defaults_for_omitted parameter
 # and explictly set every required parameter in the task defintion to ensure reproducability.
-environment_name="6.0 Domino Standard Environment Py3.10 R4.4"
-environment_revision_id="6732709a37394436a080d8ac"
+# These are the additional parameters that need to be explicitly set of each task. 
+
+environment_name="6.1 Domino Standard Environment Py3.10 R4.5"  # Change to the name of your deployments Domino Standard Environment
+environment_revision_id="68653784b616683d10d9549c"              # Change to the latest revision ID of your deployments Domino Standard Environment
 GitRef_type="branches"
 GitRef_value="main"
 volume_size_gib=10
-dfs_repo_commit_id="210c74f00622f1e509e8a3fed280b11bd05ff9f9"
+dfs_repo_commit_id="210c74f00622f1e509e8a3fed280b11bd05ff9f9"   # Change to the latest commit ID of the Artifacts file system in your project
 
 
-# Set if you want caching on or off. 
-cache=False
+# Set if you want caching on or off for all your tasks.
+cache=True
 
 # This calls the Artifact library, to create two named Flow Artifacts that we can label our merged data and model files as. 
 DataArtifact = Artifact("Merged Data", DATA)
@@ -44,7 +47,7 @@ def model_training(data_path_a: str, data_path_b: str):
         inputs=[Input(name='data_path', type=str, value=data_path_a)],
         output_specs=[Output(name='datasetA', type=FlyteFile[TypeVar('csv')])],
         environment_name=environment_name,
-        #environment_revision_id=environment_revision_id,
+        environment_revision_id=environment_revision_id,
         hardware_tier_name="Small",
         dataset_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
@@ -52,10 +55,8 @@ def model_training(data_path_a: str, data_path_b: str):
         dfs_repo_commit_id=dfs_repo_commit_id,
         external_data_volumes=[],
         cache=cache,
-        cache_version="1.0",
-        use_project_defaults_for_omitted=True
+        cache_version="1.0"
     )
-
 
     task2 = run_domino_job_task(
         flyte_task_name='Load Data B',
@@ -63,7 +64,7 @@ def model_training(data_path_a: str, data_path_b: str):
         inputs=[Input(name='data_path', type=str, value=data_path_b)],
         output_specs=[Output(name='datasetB', type=FlyteFile[TypeVar('csv')])],
         environment_name=environment_name,
-        #environment_revision_id=environment_revision_id,
+        environment_revision_id=environment_revision_id,
         hardware_tier_name="Small",
         dataset_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
@@ -71,8 +72,7 @@ def model_training(data_path_a: str, data_path_b: str):
         dfs_repo_commit_id=dfs_repo_commit_id,
         external_data_volumes=[],
         cache=cache,
-        cache_version="1.0",
-        use_project_defaults_for_omitted=True
+        cache_version="1.0"
     )
 
     task3 = run_domino_job_task(
@@ -83,16 +83,15 @@ def model_training(data_path_a: str, data_path_b: str):
             Input(name='datasetB', type=FlyteFile[TypeVar('csv')], value=task2['datasetB'])],
         output_specs=[Output(name='merged_data', type=DataArtifact.File(name="merged_data.csv"))],
         environment_name=environment_name,
-        #environment_revision_id=environment_revision_id,
-        hardware_tier_name="Small",
+        environment_revision_id=environment_revision_id,
+        hardware_tier_name="Medium",
         dataset_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         volume_size_gib=volume_size_gib,
         dfs_repo_commit_id=dfs_repo_commit_id,
         external_data_volumes=[],
         cache=cache,
-        cache_version="1.0",
-        use_project_defaults_for_omitted=True
+        cache_version="1.0"
     )
 
     task4 = run_domino_job_task(
@@ -101,16 +100,15 @@ def model_training(data_path_a: str, data_path_b: str):
         inputs=[Input(name='merged_data', type=FlyteFile[TypeVar('csv')], value=task3['merged_data'])],
         output_specs=[Output(name='processed_data', type=FlyteFile[TypeVar('csv')])],
         environment_name=environment_name,
-        #environment_revision_id=environment_revision_id,
-        hardware_tier_name="Small",
+        environment_revision_id=environment_revision_id,
+        hardware_tier_name="Medium",
         dataset_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         volume_size_gib=volume_size_gib,
         dfs_repo_commit_id=dfs_repo_commit_id,
         external_data_volumes=[],
         cache=cache,
-        cache_version="1.0",
-        use_project_defaults_for_omitted=True
+        cache_version="1.0"
     )
 
     task5 = run_domino_job_task(
@@ -121,16 +119,15 @@ def model_training(data_path_a: str, data_path_b: str):
             Input(name='num_estimators', type=int, value=100)],
         output_specs=[Output(name='model', type=ModelArtifact.File(name="model.pkl"))],
         environment_name=environment_name,
-        #environment_revision_id=environment_revision_id,
-        hardware_tier_name="Small",
+        environment_revision_id=environment_revision_id,
+        hardware_tier_name="Large",
         dataset_snapshots=[],
         main_git_repo_ref=GitRef(Type=GitRef_type, Value=GitRef_value),
         volume_size_gib=volume_size_gib,
         dfs_repo_commit_id=dfs_repo_commit_id,
         external_data_volumes=[],
         cache=cache,
-        cache_version="1.0",
-        use_project_defaults_for_omitted=True
+        cache_version="1.0"
     )
 
     return 
